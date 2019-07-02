69b0e9962f434adb965276743e201df38ce48ee3
Rustam <kiksmileip@gmail.com>
2019-07-01 21:10:44 -0400

    Added text-to-binary.py

---
+++ text-to-binary.py
@@ -0,0 +1,26 @@
+text_input = list(input("Enter text: "))
+
+unicode = []
+binary = []
+
+try:
+    
+    for i in text_input:
+        unicode.append(ord(i))
+        
+    for k in unicode:
+        num = bin(k)[2:]
+        binary.append(num)
+
+    for j in range(len(binary)):
+        while len(str(binary[i])) % 8 != 0:
+            binary[j] = "0" + binary[j]
+
+    result = ''.join(binary)
+       
+    print(result)
+
+except:
+    
+    print("Error: could not convert")
+
