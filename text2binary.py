c84961479e993c855c4b5cf293b829dc5478f8bc
Rustam <kiksmileip@gmail.com>
2019-07-01 21:25:56 -0400

    Added text2binary.py

--- text2binary.py
+++ text2binary.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 text_input = list(input("Enter text: "))
 
 unicode = []
@@ -13,7 +14,7 @@ try:
         binary.append(num)
 
     for j in range(len(binary)):
-        while len(str(binary[i])) % 8 != 0:
+        while len(str(binary[j])) % 8 != 0:
             binary[j] = "0" + binary[j]
 
     result = ''.join(binary)
